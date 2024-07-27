53efbdc08674cce06da66706e9e5ff4068901f9b
Aasem <aasem.hany@gmail.com>
2024-07-27 20:59:02 +0300

    Initial Commit

---
+++ Doctor Profile/Doctor_ProfileApp.swift
@@ -0,0 +1,17 @@
+//
+//  Doctor_ProfileApp.swift
+//  Doctor Profile
+//
+//  Created by Aasem Hany on 27/07/2024.
+//
+
+import SwiftUI
+
+@main
+struct Doctor_ProfileApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
